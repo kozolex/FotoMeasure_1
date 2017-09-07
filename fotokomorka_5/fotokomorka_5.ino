a14079f19e170922d6cf0dcfb2c3984f3707f6c3
kozolex <kozolex@gmail.com>
2017-09-07 11:24:15 +0200

    Start_project

---
+++ fotokomorka_5/fotokomorka_5.ino
@@ -0,0 +1,180 @@
+/*
+  Program odmierza czas dla zawodników, wykorzystując fotocele
+  Zasilanie 12 V stabilizowane
+  2x Sensor optyczny WL170-P132
+*/
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_NeoPixel.h>
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, 6, NEO_GRB + NEO_KHZ800);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+#define sensor1 9
+#define sensor2 10
+
+float sensorDistance = 0.2;  //Odległośc pomiędzy sensorami  
+int delayTime = 1000 ;
+unsigned long sensor1Time, sensor1TimeEnd;
+unsigned long sensor2Time, sensor2TimeEnd;
+unsigned long personTime, startTime;
+int licz = 0;
+bool sensor1Active = 0, sensor2Active = 0;
+byte sensor1Done = 0;
+bool onePerson = 0;
+byte cursorX = 0;
+byte cursorY = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pixels.begin();
+  pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.print("HI");
+
+  //Start Sequence
+  pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+  pixels.show();
+  Serial.println("3");
+  delay(delayTime);
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+  pixels.show();
+  Serial.println("2");
+  delay(delayTime);
+  pixels.setPixelColor(0, pixels.Color(0, 0, 255));
+  pixels.show();
+  Serial.println("1");
+  delay(delayTime);
+  pixels.setPixelColor(0, pixels.Color(255, 255, 255));
+  pixels.show();
+  delay(delayTime);
+  pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  pixels.show();
+  Serial.println("START");
+  startTime  = millis();
+}
+
+void loop() {
+  //SENSOR 1
+  if (digitalRead(sensor1) == 1 && sensor1Active == 0 && onePerson == 0)
+  {
+    sensor1Time = millis() - startTime;
+    onePerson = 1;
+    sensor1Active = 1;
+    pixels.setPixelColor(0, pixels.Color(255, 255, 255));
+    pixels.show();
+    licz++;
+    Serial.print ("S1 = "); Serial.println (sensor1Time);
+  }
+  else if (digitalRead(sensor1) == 0 && sensor1Active == 1 && onePerson == 1)
+  {
+    sensor1TimeEnd = millis() - startTime;
+    sensor1Active = 0;
+    Serial.print ("S1E= "); Serial.println (sensor1TimeEnd);
+  }
+  //SENSOR 2
+
+  if (digitalRead(sensor2) == 1 && onePerson == 1 && sensor2Active == 0)
+  {
+
+    sensor2Time = millis() - startTime;
+    sensor2Active = 1;
+    Serial.print ("S2 = "); Serial.println (sensor2Time);
+  }
+  else if (digitalRead(sensor2) == 0 && onePerson == 1 && sensor1Active == 0 && sensor2Active == 1)
+  {
+    sensor2TimeEnd = millis() - startTime;
+    Serial.print ("S2E= "); Serial.println (sensor2TimeEnd);
+    onePerson = 0;
+    sensor2Active = 0;
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
+
+    //Decyzja
+    //obiekt mniejszy od odległości między sensorami
+    if (sensor1Time < sensor2Time &&  sensor1TimeEnd < sensor2TimeEnd)
+    {
+      Serial.print("Zawodnik nr: ");
+      Serial.print(licz);
+      Serial.print("\tCzas: ");
+      Serial.print(sensor1Time / 1000);
+      Serial.print("s ");
+      Serial.print(sensor1Time % 1000);
+      Serial.println("ms ");
+      Serial.print("Wielkosc: ");
+      Serial.print((sensorDistance / (sensor2Time - sensor1Time) * 1000) * (sensor1TimeEnd - sensor1Time) / 1000 );
+      Serial.print("m\t");
+      Serial.print("Szybkosc: ");
+      Serial.print((sensorDistance / (sensor2Time - sensor1Time) * 3600));
+      Serial.print("km/h");
+      Serial.println(""); Serial.println("");
+
+      lcd.setCursor(cursorX, cursorY);
+      cursorY++;
+      if (cursorY == 4) 
+      {
+        cursorY = 0;
+        cursorX += 11;
+        if (cursorX > 11)
+        {
+          cursorX = 0;
+          cursorY = 0;
+        }
+      }
+      lcd.print(licz);
+      lcd.print(" ");
+      lcd.print(sensor1Time / 1000);
+      lcd.print(",");
+      lcd.print(sensor1Time % 1000);  
+      //lcd.print("s");    
+      delay(2000);
+    }
+  }
+  pixels.show();
+
+
+
+
+
+}
+/*
+
+
+
+   Serial.println ("S2 - 0");
+    Serial.print("sT1: ");
+    Serial.print("sT1E: ");
+    Serial.print(sensor1TimeEnd);
+    Serial.println(" ms");
+
+    Serial.print("sT2: ");
+    Serial.print(sensor2Time);
+    Serial.print(" ms\t");
+    Serial.print("sT2E: ");
+    Serial.print(sensor2TimeEnd);
+    Serial.println(" ms");
+
+    Serial.print("Zawodnik nr: ");
+    Serial.print(licz);
+    Serial.print("\tCzas: ");
+    Serial.print((millis() - sensor1Time) / 1000);
+    Serial.print("s ");
+    Serial.print((millis() - sensor1Time) % 1000);
+    Serial.println("ms ");
+    Serial.print("Szerokosc: ");
+    Serial.print((sensorDistance / (sensor1TimeEnd - sensor1Time) * 1000) * (sensor1TimeEnd - sensor1Time) / 1000 );
+    Serial.print("m");
+    Serial.print("\t");
+    Serial.print("Szybkosc: ");
+    Serial.print(sensorDistance / (sensor1TimeEnd - sensor1Time) * 1000);
+    Serial.print("m/s");
+    Serial.print("\t");
+    Serial.print("Szybkosc: ");
+    Serial.print((sensorDistance / (sensor1TimeEnd - sensor1Time) * 3600));
+    Serial.print("km/h");
+    Serial.println(""); Serial.println("");
+
+*/
+
